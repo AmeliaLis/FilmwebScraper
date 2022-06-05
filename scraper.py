5bac88366d3f2e65b67bbf56fa4971de09c1f040
AmeliaLis <s221423@student.uek.krakow.pl>
2022-06-05 23:19:51 +0200

    05.06.2022 5. commit

--- scraper.py
+++ scraper.py
@@ -3,36 +3,87 @@ import requests
 import json
 from bs4 import BeautifulSoup
 
-url = "https://www.filmweb.pl/film/Doktor+Strange+w+multiwersum+obłędu-2022-836440"
+url = input("Enter link from Filmweb: ")
 response = requests.get(url)
-
+id_url = url.split("-")[-1]
 page = BeautifulSoup(response.text, "html.parser")
 
-kind = page.select_one("div.filmCoverSection__type").get_text()
+try:
+    kind = page.find("div", {"class":"filmCoverSection__type"}).get_text()
+except:
+    kind = ""
+
 title_polish = page.select_one("h1.filmCoverSection__title").get_text()
-#original_titile = page.select_one("div.filmCoverSection__originalTitle").get_text() #zastanowię sie nad tym, bo nie wszystkie filmy poskie mają angieslki tytuł
-release_date = page.select_one("span.block").get_text().strip()
-span_genres = page.find("div", {"itemprop":"genre"}).find_all("span")
 
-genres = []
-for span_genre in span_genres:
-    if span_genre.get_text() != " / ":
-        genres.append(span_genre.get_text())
+try:
+    original_title = page.select_one("div.filmCoverSection__originalTitle").get_text() 
+except:
+    original_title = ""
 
-description = page.select_one("div.filmPosterSection__plot").find("span").get_text()
-public_rating = page.select_one("span.filmRating__rateValue").get_text()
+try:
+    release_date = page.select_one("span.block").get_text().strip()
+except:
+    release_date = None
+
+if release_date == None:
+    seasons = page.find("div", {"data-source":"seasonsOrYears"}).get_text()
+    seasons = json.loads(seasons)
+else:
+    seasons = {
+        "seasons": [],
+    }
+
+try:
+    span_genres = page.find("div", {"itemprop":"genre"}).find_all("span")
+    genres = []
+    for span_genre in span_genres:
+        if span_genre.get_text() != " / ":
+            genres.append(span_genre.get_text())
+except:
+    genres = []
+
+try:
+    div_genres = page.find("div", {"itemprop":"genre"}).find_all("a")
+    genres = []
+    for a in div_genres:
+        genres.append(a.get_text())
+except:
+    genres = []
+
+try:
+    description = page.select_one("div.filmPosterSection__plot").find("span").get_text()
+except:
+    try:
+        description = page.find("span", {"itemprop":"description"}).get_text().strip()
+    except:
+        description = ""
+
+public_rating = page.select_one("span.filmRating__rateValue").get_text() #try find
 number_of_public_rating = page.select_one("span.filmRating__count").get_text().strip()
 
-places = page.select_one("div.filmPosterSection__info.filmInfo > div:nth-child(11)").find_all("span")
-places_where = []
-for place in places:
-    if place.get_text() != " / ":
-        places_where.append(place.get_text())
+try:
+    critics = page.find("div", {"data-source":"criticRatingData"}).get_text()
+    critics = json.loads(critics)
+except:
+    critics = None
 
-director = page.find("div", {"data-type":"directing-info"}).find("span", {"itemprop":"name"}).get_text()
-scriptwriter = page.find("div", {"data-type":"screenwriting-info"}).find("span", {"itemprop":"name"}).get_text()
+try:
+    director = page.find("div", {"data-type":"directing-info"}).find("span", {"itemprop":"name"}).get_text()
+except:
+    director = ""
 
-##### aktorzy
+try:
+    div_creators = page.find("div", {"data-type":"creators-info"}).find_all("span", {"itemprop":"name"})
+    creators = []
+    for div_creator in div_creators:
+        creators.append(div_creator.get_text())
+except:
+    creators = ""
+
+try:
+    scriptwriter = page.find("div", {"data-type":"screenwriting-info"}).find("span", {"itemprop":"name"}).get_text()
+except:
+    scriptwriter = None
 
 pageActorsUrl = f"{url}" + "/cast/actors"
 response = requests.get(pageActorsUrl)
@@ -48,6 +99,8 @@ for index in range(5):
 ##### opinie
 pageOpinionsUrl = url + "/discussion"
 
+allOpinions = []
+
 while(pageOpinionsUrl):
     response = requests.get(pageOpinionsUrl)
     pageOpinions = BeautifulSoup(response.text, "html.parser")
@@ -55,58 +108,68 @@ while(pageOpinionsUrl):
     opinions = pageOpinions.find_all("li", {"class":"forumSection__item"})
 
     for opinion in opinions:
-
         title = opinion.find("a", {"class":"forumSection__itemLink"}).get_text().strip()
-        print(title)
-
         try:
             review = opinion.find("p", {"class":"forumSection__topicText"}).get_text().strip()
             if review == "Uwaga Spoiler! Ten temat może zawierać treści zdradzające fabułę.":
                 review = ""
-            print(review)
         except:
             review = None
 
         try:
             author = opinion.find("span", {"class":"forumSection__authorName"}).get_text().strip()
-            print(author)
         except:
             author = "użytkownik usunięty"
 
         try:
             stars = opinion.find("span", {"class":"forumSection__starsNo"}).get_text().strip()
-            if int(stars) > 1:
-                print(stars + " stars")
-            else:
-                print(stars + " star")
         except:
             stars = None
 
         date = opinion.find("time").get_text()
-        print(date)
 
         try:
             likes = opinion.find("span", {"class":"plusMinusWidget__count"}).get_text().strip()
-            if int(likes) > 1:
-                print(likes + " likes")
-            else:
-                print(likes + " like")
         except:
             likes = None
-        
+
         try:
             comments = opinion.find("span", {"class":"forumSection__commentsCount"}).get_text().strip()
-            if int(likes) > 1:
-                print(likes + " comments")
-            else:
-                print(likes + " comment")
         except:
             likes = None
-        print("\n")
+    allOpinions.append({
+        "title": title,
+        "review": review,
+        "author": author,
+        "stars": stars,
+        "date": date,
+        "likes": likes,
+        "comments": comments,
+    })
     try:
         pageOpinionsUrl = "https://www.filmweb.pl" + pageOpinions.find("a",{"title" : "następna"}).get("href")
-        print(pageOpinionsUrl)
     except:
         pageOpinionsUrl = None
 
+filmweb_data = {
+    "info": {
+        "title_polish": title_polish,
+        "kind": kind,
+        "original_title": original_title,
+        "release_date": release_date,
+        "seasons": seasons["seasons"],
+        "genres": genres,
+        "description": description,
+        "public_rating": public_rating,
+        "number_of_public_rating": number_of_public_rating,
+        "critics": critics,
+        "director": director,
+        "creators": creators,
+        "scriptwriter": scriptwriter,
+        "actorsArray": actorsArray,
+    },
+    "allOpinions": allOpinions,
+}
 
+with open(f"opinions/{id_url}.json", "w", encoding ="UTF-8") as jf:
+    json.dump(filmweb_data, jf, indent=4, ensure_ascii=False)
