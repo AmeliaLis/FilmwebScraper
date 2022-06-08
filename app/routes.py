601636a322d8df336fb7c1c1e9656e4466c4a57b
AmeliaLis <s221423@student.uek.krakow.pl>
2022-06-08 21:58:23 +0200

    08.06.2022 ogólny layout stron 9. commit

--- app/routes.py
+++ app/routes.py
@@ -4,3 +4,11 @@ from flask import render_template
 @app.route('/')
 def index():
     return render_template("index.html.jinja")
+
+@app.route("/author")
+def author():
+    return render_template("author.html.jinja")
+
+@app.route("/extract")
+def extract():
+    return render_template("extract.html.jinja")
